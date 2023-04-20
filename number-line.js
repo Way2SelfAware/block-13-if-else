272c1d0f1e1a5638c0b53a6089b6619100e0bc79
Way2SelfAware <122562668+Way2SelfAware@users.noreply.github.com>
2023-04-20 12:38:06 -0700

    Add files via upload
    finished Number Line

---
+++ number-line.js
@@ -0,0 +1,59 @@
+// num1 + num2 //
+// 50 + 51 = "101 is greater than 100" //
+let num1 = 50;
+let num2 = 51;
+let sum1 = num1 + num2;
+if (sum1 > 100) {
+  console.log(sum1 + " is greater than 100");
+} else {
+  console.log("You done messed up");
+}
+// 99 + (-2) = "97 is greater than 0" //
+let num3 = 99;
+let num4 = -2;
+let sum2 = num3 + num4;
+if (sum2 > 0) {
+  console.log(sum2 + " is greater than 0");
+} else {
+  console.log("You done messed up");
+}
+
+// 0 + 101 = 	"101 is greater than 100" //
+let num5 = 0;
+let num6 = 101;
+let sum3 = num5 + num6;
+if (sum3 > 100) {
+  console.log(sum3 + " is greater than 100");
+} else {
+  console.log("You done messed up");
+}
+
+// 500 + (-500) = "0 is equal to 0" //
+let num7 = 500;
+let num8 = -500;
+let sum4 = num7 + num8;
+if (sum4 == 0) {
+  console.log(sum4 + " is equal to 0");
+} else {
+  console.log("You done messed up");
+}
+
+// (-1000) + 0 = "-1000 is a negative number" //
+let num9 = -1000;
+let num10 = 0;
+let sum5 = num9 + num10;
+if (sum5 < 0) {
+  console.log(sum5 + " is a negative number");
+} else {
+  console.log("You done messed up");
+}
+
+// (-5) + 0 = 	"-5 is a negative number" //
+let num11 = -5;
+let num12 = 0;
+let sum6 = num11 + num12;
+if (sum6 < 0) {
+  console.log(sum6 + " is a negative number");
+} else {
+  console.log("You done messed up");
+}
