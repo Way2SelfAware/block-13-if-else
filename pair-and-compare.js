f80698e571c93cd58654eff6727eada0bfbd2c53
Way2SelfAware <122562668+Way2SelfAware@users.noreply.github.com>
2023-04-20 13:04:29 -0700

    Add files via upload
    finished pair and compare

---
+++ pair-and-compare.js
@@ -0,0 +1,45 @@
+//if/else ||//
+//if at least 1 true print true//
+
+//value 1//
+//"cat"=="cat" || 6=="6"//
+if ("cat" == "cat" || 6 == "6") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+//value 2//
+//"five"==5 || "dog"=="dawg"//
+if (5 == "five" || "dog" == "dawg") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+//value 3//
+//0==false || "horse=="horse"//
+if (0 == false || "horse" == "horse") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+//value 4//
+//"eight"=="eight" || "ate"=="ate"//
+if (8 == "eight" || "ate" == "ate") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+//value 5//
+//11=="eleven" || "four"=="for"//
+if (11 == "eleven" || "four" == "for") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+//value 6//
+//"cake"=="cake" || "pie"=="pie"//
+if ("cake" == "cake" || "pie" == "pie") {
+  console.log(true);
+} else {
+  console.log(false);
+}
