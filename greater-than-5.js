9c3de8987457b2264a35880c82a1e472a4ffc87e
Way2SelfAware <122562668+Way2SelfAware@users.noreply.github.com>
2023-04-20 12:49:48 -0700

    Add files via upload
    finished greater than 5

---
+++ greater-than-5.js
@@ -0,0 +1,52 @@
+//print true if both conditions are >= 5//
+//num1 && num2 >= 5//
+//if true print true//
+//if false print false//
+
+let num1 = 5;
+let num2 = 6;
+if (num1 && num2 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let num3 = 10;
+let num4 = 11;
+if (num3 && num4 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let num5 = 0;
+let num6 = 0;
+if (num5 && num6 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let num7 = 1000;
+let num8 = -1000;
+if (num7 && num8 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let num9 = 6;
+let num10 = 4;
+if (num9 && num10 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let num11 = 5;
+let num12 = 5;
+if (num11 && num12 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
